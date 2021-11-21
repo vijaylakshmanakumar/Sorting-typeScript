961e6338169b8f2a4a81406314aa2aa739af8646
vijaylakshmanakumar <babyv1943@gmail.com>
2021-11-21 14:53:04 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 var number = [290, 28, 23, 123, 21, 3, 4];
-var names = ['Hello', 'String', 'Welcome', 'to', 'all'];
+var names = ['Hello', 'String', 'Welcome', 'To', 'All', 'I'];
 
 var val = number.sort();
 console.log(val);
